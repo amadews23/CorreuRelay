3a6a56636fa2849b2f80a2aa5ea50331f4bd8f49
Bartolomé Vich Lozano <tolo@tovilo.es>
2019-12-12 14:57:26 +0100

    Update configurar_correo_postfix_sasl_passwd.sh

--- gestionador/scripts/configurar_correo_postfix_sasl_passwd.sh
+++ gestionador/scripts/configurar_correo_postfix_sasl_passwd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#sasl_password_file='/etc/postfix/sasl/sasl_passwd'
-sasl_password_file='sasl_passwd'
+sasl_password_file='/etc/postfix/sasl/sasl_passwd'
+
 renombrado=-$(date +"%d-%h-%y_%H:%M:%S").old
 
 #Realizamos copia por si algo sale mal
@@ -9,4 +9,5 @@ cp $sasl_password_file $sasl_password_file${renombrado}
 
 echo $1'	'$1':'$2 > $sasl_password_file
 cat $sasl_password_file${renombrado} >> $sasl_password_file
-#postmap ${sasl_password_file}
+
+postmap ${sasl_password_file}
