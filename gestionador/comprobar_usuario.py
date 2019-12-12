e807a529850f1c66c2f67b06749bc624dc23ccda
Bartolomé Vich Lozano <tolo@tovilo.es>
2019-12-12 14:36:54 +0100

    Add files via upload

---
+++ gestionador/comprobar_usuario.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+import model
+
+def comprobar_usuario_bd (usuario_bd ):
+	usuarios_bd = model.get_cuentas_usuarios()
+	for usuario in usuarios_bd:
+
+		if usuario.usuario == usuario_bd:
+			print "El usuario ya está dado de alta en la Base de Datos"
+			return True;
+	return False;
+
+
+def comprobar_usuario_unix( usuario_unix):
+
+	comando = "/bin/bash scripts/./comprobar_usuario_unix.sh "+usuario_unix
+
+	ejecucion = os.popen(comando)
+
+	'''Quitamos el espacio final del resultado devuelto mediante expresion regular''' 
+	resultado = re.sub(r"\s+$", "", ejecucion.read(), flags=re.UNICODE) 
+
+	if resultado == "1":
+		print "Es un usuario con Login"
+		return True
+
+	if resultado == "2":
+		print "Es un usuario de Sistema"
+		return True
+
+	if resultado == "0":
+		print "El usuario No Existe"
+		return False
+
+#comprobar_usuario_unix("amadews")
+#comprobar_usuario_unix("bin")
+#comprobar_usuario_unix("queso")
