8a6c134a031c867d970f9d2a8d7d2f711dc4abb5
Bartolomé Vich Lozano <tolo@tovilo.es>
2019-12-12 14:49:15 +0100

    Update comprobar_correo_postfix_relayhost_map.sh

--- gestionador/scripts/comprobar_correo_postfix_relayhost_map.sh
+++ gestionador/scripts/comprobar_correo_postfix_relayhost_map.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#sender_depend_relay_hostmaps_file='/etc/postfix/relay_hostmap'
-sender_depend_relay_hostmaps_file='relay_hostmap'
+sender_depend_relay_hostmaps_file='/etc/postfix/relay_hostmap'
+
 if [[ $(awk '{print $1}' $sender_depend_relay_hostmaps_file | grep $1) = $1 ]]; then
         #echo "El correo ya existe."
 	echo 1
