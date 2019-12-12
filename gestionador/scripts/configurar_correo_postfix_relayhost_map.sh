593abdb9f0f911253ce7ba79e54aaab09d769384
Bartolomé Vich Lozano <tolo@tovilo.es>
2019-12-12 14:56:44 +0100

    Update configurar_correo_postfix_relayhost_map.sh

--- gestionador/scripts/configurar_correo_postfix_relayhost_map.sh
+++ gestionador/scripts/configurar_correo_postfix_relayhost_map.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#sender_depend_relay_hostmaps_file='/etc/postfix/relay_hostmap'
-sender_depend_relay_hostmaps_file='relay_hostmap'
-smtp_remoto=smtp.strato.com
+sender_depend_relay_hostmaps_file='/etc/postfix/relay_hostmap'
+
+smtp_remoto=smtp.miproveedor.com
 puerto_smtp_remoto=587
 renombrado=-$(date +"%d-%h-%y_%H:%M:%S").old
 
@@ -12,4 +12,4 @@ cp $sender_depend_relay_hostmaps_file $sender_depend_relay_hostmaps_file${renomb
 echo $1' ['$smtp_remoto']:'$puerto_smtp_remoto > $sender_depend_relay_hostmaps_file
 cat $sender_depend_relay_hostmaps_file${renombrado} >> $sender_depend_relay_hostmaps_file
 
-#postmap ${sender_depend_relay_hostmaps_file}
+postmap ${sender_depend_relay_hostmaps_file}
