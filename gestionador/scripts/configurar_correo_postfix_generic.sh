cc7d6201959d5576004a2be59f9d52ffe76fa1c9
Bartolomé Vich Lozano <tolo@tovilo.es>
2019-12-12 14:54:11 +0100

    Update configurar_correo_postfix_generic.sh

--- gestionador/scripts/configurar_correo_postfix_generic.sh
+++ gestionador/scripts/configurar_correo_postfix_generic.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#smtp_generic_maps_file='/etc/postfix/generic'
-smtp_generic_maps_file='generic'
+smtp_generic_maps_file='/etc/postfix/generic'
 
 mi_host=$(cat /etc/hostname)
 renombrado=-$(date +"%d-%h-%y_%H:%M:%S").old
@@ -12,4 +11,4 @@ cp $smtp_generic_maps_file $smtp_generic_maps_file${renombrado}
 echo $1'@'$mi_host'	'$2 > $smtp_generic_maps_file
 cat $smtp_generic_maps_file${renombrado} >> $smtp_generic_maps_file
 
-#postmap $smtp_generic_maps_file
+postmap $smtp_generic_maps_file
