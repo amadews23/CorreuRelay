59ce73ec037c11fe8018f04e01ba4da7a811d163
Bartolomé Vich Lozano <tolo@tovilo.es>
2019-12-12 13:57:17 +0100

    Create comprobar_correo_fetchmail.sh

---
+++ gestionador/scripts/comprobar_correo_fetchmail.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#fetchmailrc_file='/etc/fetchmailrc'
+fetchmailrc_file='fetchmailrc'
+
+if [[ $(awk '{print $2}' $fetchmailrc_file | grep $1) = $1 ]]; then
+        #echo "El correo ya existe."
+	echo 1
+else
+        #echo "El correo NO existe."
+	echo 0
+fi
