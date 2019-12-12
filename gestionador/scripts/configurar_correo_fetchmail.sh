40195f2b46fb435ee6e81831e037cb9a96b86531
Bartolomé Vich Lozano <tolo@tovilo.es>
2019-12-12 14:51:58 +0100

    Update configurar_correo_fetchmail.sh

--- gestionador/scripts/configurar_correo_fetchmail.sh
+++ gestionador/scripts/configurar_correo_fetchmail.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#fetchmailrc_file='/etc/fetchmailrc'
-fetchmailrc_file='fetchmailrc'
-pop_remoto='pop3.strato.com'
+fetchmailrc_file='/etc/fetchmailrc'
+
+pop_remoto='pop3.miproveedor.com'
 
 renombrado=-$(date +"%d-%h-%y_%H:%M:%S").old
 
 #Realizamos copia por si algo sale mal
 cp $fetchmailrc_file $fetchmailrc_file${renombrado}
+
 echo ' ' >> $fetchmailrc_file
 echo 'poll '$pop_remoto >> $fetchmailrc_file
 echo '	proto POP3' >> $fetchmailrc_file
