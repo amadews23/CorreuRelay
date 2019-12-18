d7a14bb6e7d1d55ee8d29b1b120606bd420a5f1e
Bartolomé Vich Lozano <tolo@tovilo.es>
2019-12-18 18:38:59 +0100

    Update crear_usuario.py

--- gestionador/crear_usuario.py
+++ gestionador/crear_usuario.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Archivo: crear_usuario.py
+# Autor: Bartolomé Vich Lozano
+# Email: amadews23@hotmail.com
+# Fecha: 15 de Diciembre de 2019
+# Descripción: Contiene una función para crear un usuario unix pásandole el nombre de usuario y password.
+#
+
 import os
 import subprocess
 
